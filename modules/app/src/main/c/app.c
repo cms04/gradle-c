763207202e0294a625be69b8e6d806318a17d755
cms04 <Christian-Morbach@hotmail.com>
2024-05-09 17:45:10 +0200

    libraries

--- modules/app/src/main/c/app.c
+++ modules/app/src/main/c/app.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "app.h"
+#include "util.h"
 
 int main() {
   printf("Hello World\n");
+  printf("%d\n", the_number());
   return EXIT_SUCCESS;
 }
---
+++ modules/libs/common/src/main/c/util.c
@@ -0,0 +1,5 @@
+#include "util.h"
+
+int the_number() {
+  return 42;
+}
